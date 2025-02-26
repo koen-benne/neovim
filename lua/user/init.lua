72db2804f9c252096ea6851bf75e72d5a40bea5e
Koen Benne <koen.benne@gmail.com>
2025-02-26 15:06:26 +0100

    yuhh

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -3,7 +3,7 @@ require('user.opts_and_keys')
 
 -- NOTE: register an extra lze handler with the spec_field 'for_cat'
 -- that makes enabling an lze spec for a category slightly nicer
-require("lze").register_handlers(require('nixCatsUtils.lzUtils').for_cat)
+require("lze").register_handlers(require('utils.lze').for_cat)
 
 -- NOTE: general plugins
 require("user.plugins")
