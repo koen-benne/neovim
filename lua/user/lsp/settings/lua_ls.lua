a905daccc78a48abd69f8d783abde77d6c2dccb2
Koen Benne <koen.benne@gmail.com>
2025-12-17 10:17:50 +0100

    i need dis

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -5,6 +5,16 @@ return {
         ignoreComments = true,
       },
       signatureHelp = { enabled = true },
+      diagnostics = {
+        globals = { 'nixCats', 'vim' },
+        disable = { 'missing-fields' },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
     },
     telemetry = { enabled = false },
   },
