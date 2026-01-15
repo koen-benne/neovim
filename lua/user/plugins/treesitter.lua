32f734e9b07356e2451774106cb724914e55f1b3
Koen Benne <koen.benne@gmail.com>
2026-01-15 16:07:15 +0100

    update

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -5,6 +5,9 @@ return {
     "nvim-treesitter",
     for_cat = 'general.treesitter',
     event = "DeferredUIEnter",
+    load = function(name)
+      vim.cmd.packadd(name)
+    end,
     after = function (plugin)
       -- Load dependent plugins AFTER nvim-treesitter is loaded
       vim.cmd.packadd("nvim-treesitter-context")
@@ -13,66 +16,67 @@ return {
 
       vim.g.skip_ts_context_comment_string_module = true
 
-      require('nvim-treesitter.configs').setup {
-        sync_install = false,
-        auto_install = false,
+      -- nvim-treesitter v0.10+ uses a simplified API
+      -- Syntax highlighting is now enabled by default in Neovim
+      vim.treesitter.language.register('bash', 'sh')
 
-        highlight = { enable = true, },
-        indent = { enable = false, },
-        incremental_selection = {
+      -- Configure textobjects separately (now standalone)
+      require('nvim-treesitter-textobjects').setup {
+        select = {
           enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
           keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<M-space>',
+            -- You can use the capture groups defined in textobjects.scm
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
           },
         },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']]'] = '@class.outer',
           },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
           },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-            },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<leader>A'] = '@parameter.inner',
           },
         },
       }
+
+      -- Incremental selection (built into Neovim treesitter now)
+      vim.keymap.set('n', '<c-space>', function()
+        vim.cmd('normal! v')
+        require('vim.treesitter').select_range()
+      end, { desc = 'Treesitter: Init selection' })
+      vim.keymap.set('x', '<c-space>', function()
+        require('vim.treesitter').select_range()
+      end, { desc = 'Treesitter: Expand selection' })
+      vim.keymap.set('x', '<M-space>', function()
+        require('vim.treesitter').select_range({ parent = true })
+      end, { desc = 'Treesitter: Shrink selection' })
 
       require('treesitter-context').setup {
         max_lines = 3,
