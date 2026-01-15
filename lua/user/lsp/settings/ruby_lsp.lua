9d48f3e77b282d587e25d5cd19c7206fc15ac520
Koen Benne <koen.benne@gmail.com>
2026-01-15 17:08:24 +0100

    updatey?

--- lua/user/lsp/settings/ruby_lsp.lua
+++ lua/user/lsp/settings/ruby_lsp.lua
@@ -1,7 +1,11 @@
 return {
   filetypes = { "ruby" },
-  root_dir = function(fname)
-    return vim.fs.root(fname, { "Gemfile", ".git" })
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root = vim.fs.root(fname, { "Gemfile", ".git" })
+    if root then
+      on_dir(root)
+    end
   end,
   init_options = {
     formatter = "auto",
