3cd279d9e264befac3b44179edfa05ed96e61845
Koen Benne <koen.benne@gmail.com>
2026-04-29 14:07:35 +0200

    fix

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,77 +3,54 @@ if nixInfo(false, "info", "lspDebugMode") then
 end
 
 require('lze').load {
-  -- nvim-lspconfig is loaded purely for its lsp/*.lua default configs,
-  -- which Neovim 0.11+ picks up automatically. We do NOT call require('lspconfig').
   {
-    "nvim-lspconfig",
+    "lsp-setup",
     for_cat = 'general',
-    event = "FileType",
-  },
-  {
-    "SchemaStore.nvim",
-    for_cat = 'general',
-    event = "FileType",
-  },
-  {
-    "ts-error-translator.nvim",
-    for_cat = 'general',
-    event = "FileType",
+    on_plugin = { "blink.cmp" },
+    load = function(_)
+      vim.cmd.packadd("nvim-lspconfig")
+      vim.cmd.packadd("SchemaStore.nvim")
+      vim.cmd.packadd("ts-error-translator-nvim")
+    end,
     after = function()
       require('ts-error-translator').setup()
+      require('user.lsp.handlers').setup()
+
+      vim.lsp.config('*', {
+        flags = {
+          debounce_text_changes = 150,
+        },
+        on_attach = require('user.lsp.handlers').on_attach,
+        capabilities = require('user.lsp.handlers').capabilities,
+      })
+
+      vim.lsp.config('lua_ls',        require('user.lsp.settings.lua_ls'))
+      vim.lsp.config('rust_analyzer', require('user.lsp.settings.rust'))
+      vim.lsp.config('jsonls',        require('user.lsp.settings.jsonls'))
+      vim.lsp.config('basedpyright',  require('user.lsp.settings.basedpyright'))
+      vim.lsp.config('emmet_ls',      require('user.lsp.settings.emmet_ls'))
+      vim.lsp.config('intelephense',  require('user.lsp.settings.intelephense'))
+      vim.lsp.config('nixd',          require('user.lsp.settings.nixd'))
+      vim.lsp.config('ruby_lsp',      require('user.lsp.settings.ruby_lsp'))
+
+      vim.lsp.enable({
+        'ts_ls',
+        'eslint',
+        'cssls',
+        'html',
+        'emmet_ls',
+        'gopls',
+        'nixd',
+        'lua_ls',
+        'rust_analyzer',
+        'basedpyright',
+        'ruff',
+        'bashls',
+        'fish_lsp',
+        'intelephense',
+        'jsonls',
+        'ruby_lsp',
+      })
     end,
   },
 }
-
--- Set up LSP handlers/diagnostics config
-require('user.lsp.handlers').setup()
-
--- Global defaults for all servers
-vim.lsp.config('*', {
-  flags = {
-    debounce_text_changes = 150,
-  },
-  on_attach = require('user.lsp.handlers').on_attach,
-  capabilities = require('user.lsp.handlers').capabilities,
-})
-
--- Server-specific overrides (only where we differ from lspconfig defaults)
-vim.lsp.config('lua_ls',        require('user.lsp.settings.lua_ls'))
-vim.lsp.config('rust_analyzer', require('user.lsp.settings.rust'))
-vim.lsp.config('jsonls',        require('user.lsp.settings.jsonls'))
-vim.lsp.config('basedpyright',  require('user.lsp.settings.basedpyright'))
-vim.lsp.config('emmet_ls',      require('user.lsp.settings.emmet_ls'))
-vim.lsp.config('intelephense',  require('user.lsp.settings.intelephense'))
-vim.lsp.config('nixd',          require('user.lsp.settings.nixd'))
-vim.lsp.config('ruby_lsp',      require('user.lsp.settings.ruby_lsp'))
-
--- Enable servers. nvim-lspconfig's lsp/*.lua files provide cmd/filetypes/root_dir defaults.
-vim.lsp.enable({
-  -- React / JS / TS
-  'ts_ls',
-  'eslint',
-  'cssls',
-  'html',
-  'emmet_ls',
-  -- Go
-  'gopls',
-  -- Nix
-  'nixd',
-  -- Lua
-  'lua_ls',
-  -- Rust
-  'rust_analyzer',
-  -- Python
-  'basedpyright',
-  'ruff',
-  -- Bash
-  'bashls',
-  -- Fish
-  'fish_lsp',
-  -- PHP
-  'intelephense',
-  -- JSON (used across many filetypes)
-  'jsonls',
-  -- Ruby (binary comes from project ruby env, not nix)
-  'ruby_lsp',
-})
